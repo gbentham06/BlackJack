9ff43646f3307aea019cba6b339873dc7ea15eb7
gbentham06 <gbentham06@gmail.com>
2025-06-26 00:29:18 +0100

    Get card values + structure

--- BlackJack/Card.java
+++ BlackJack/Card.java
@@ -19,4 +19,24 @@ public class Card {
     public String toString() {
         return rank + " of " + suit;
     }
+    public int getNumericalValue(boolean aceHigh) {
+        try {
+            // Try to parse ranks 2-10 directly
+            return Integer.parseInt(rank);
+        } catch (NumberFormatException e) {
+            // If it's not a number, it must be a face card or Ace
+            switch (rank) {
+                case "Jack":
+                    return 10;
+                case "Queen":
+                    return 10;
+                case "King":
+                    return 10;
+                case "Ace":
+                    return aceHigh ? 11 : 1; // Dynamic Ace value
+                default:
+                    throw new IllegalArgumentException("Unknown card rank: " + rank);
+            }
+        }
+    }
 }
--- BlackJack/Main.java
+++ BlackJack/Main.java
@@ -1,9 +1,38 @@
 
 import java.util.LinkedList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         LinkedList<Card> cards = Deck.buildDeck();
+
+        LinkedList <Card> myhand = new LinkedList<>();
+        LinkedList <Card> dealhand = new LinkedList<>();
+
+        for (int i = 0; i < 2; i++){
+        myhand.add(cards.pop());
+        dealhand.add(cards.pop());
+        }
+
+
+        boolean mybust = false;
+        boolean dealbust = false;
+        boolean istand = false;
+        boolean dealstand = false;
+
+        while ((mybust == false && dealbust == false) && (istand == false || dealstand == false)){
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Hit or Stand:");
+            String input = sc.nextLine();
+
+            if ("h".equals(input)){
+                // hit
+            } else if ("s".equals(input)){
+                // stand
+            } else {
+                // invalid: standing by default
+            }
+        }
         
     }
 }
