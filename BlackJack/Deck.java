610ea35490d8865ad6a9b97944d29742dd3fb2a6
gbentham06 <gbentham06@gmail.com>
2025-06-25 01:01:19 +0100

    Add files via upload

--- BlackJack/Deck.java
+++ BlackJack/Deck.java
@@ -1,24 +1,28 @@
-
+import java.util.Collections;
 import java.util.LinkedList;
 
 public class Deck {
 
-    LinkedList<String> buildDeck(){
-        LinkedList<String> out = new LinkedList<>();
-        String[] suits = {"♠", "♥", "♦", "♣"};
+    public static LinkedList<Card> buildDeck() {
+        LinkedList<Card> deck = new LinkedList<>();
 
-        for (int i = 1; i < 14; i++){;
-            for (String suit : suits){
-            switch (i) {
-                case 1 -> out.add("A" + suit);
-                case 11 -> out.add("J" + suit);
-                case 12 -> out.add("Q" + suit);
-                case 13 -> out.add("K" + suit);
-                default -> out.add(Integer.toString(i) + suit);
-            }
+        String[] suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
+        String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+
+        for (String suit : suits) {
+            for (String rank : ranks) {
+                deck.add(new Card(suit, rank));
             }
         }
 
-        return out;
+        Collections.shuffle(deck);
+
+        return deck;
+    }
+
+    public static void printDeck(LinkedList<Card> deck) {
+        for (Card card : deck) {
+            System.out.println(card);
+        }
     }
 }
